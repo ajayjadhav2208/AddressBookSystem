833a570fc91a7c514ac6226635712c9e68cb4499
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-10-20 13:43:06 +0530

    Eight UC Case

--- src/com/bridgelabz/ContactInfo.java
+++ src/com/bridgelabz/ContactInfo.java
@@ -1,100 +1,89 @@
 package com.bridgelabz;
-import java.util.Scanner;
+class ContactInfo {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNo;
+    private String email;
 
- class ContactInfo {
+    public String getFirstName() {
+        return firstName;
+    }
 
-     private String firstName;
-     private String lastName;
-     private String address;
-     private String city;
-     private String state;
-     private int zip;
-     private long phoneNo;
-     private String email;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-     public String getFirstName() {
-         return firstName;
-     }
+    public String getLastName() {
+        return lastName;
+    }
 
-     public void setFirstName(String firstName) {
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-         this.firstName = firstName;
-     }
+    public String getAddress() {
+        return address;
+    }
 
-     public String getLastName() {
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-         return lastName;
-     }
+    public String getCity() {
+        return city;
+    }
 
-     public void setLastName(String lastName)
-     {
-         this.lastName = lastName;
-     }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-     public String getAddress() {
-         return address;
-     }
+    public String getState() {
+        return state;
+    }
 
-     public void setAddress(String address) {
+    public void setState(String state) {
+        this.state = state;
+    }
 
-         this.address = address;
-     }
+    public int getZip() {
+        return zip;
+    }
 
-     public String getCity() {
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
 
-         return city;
-     }
+    public long getPhoneNo() {
+        return phoneNo;
+    }
 
-     public void setCity(String city) {
-         this.city = city;
-     }
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
 
-     public String getState() {
-         return state;
-     }
+    public String getEmail() {
+        return email;
+    }
 
-     public void setState(String state) {
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-         this.state = state;
-     }
-
-     public int getZip() {
-         return zip;
-     }
-
-     public void setZip(int zip) {
-         this.zip = zip;
-     }
-
-     public long getPhoneNo() {
-
-         return phoneNo;
-     }
-
-     public void setPhoneNo(long phoneNo) {
-
-         this.phoneNo = phoneNo;
-     }
-
-     public String getEmail() {
-         return email;
-     }
-
-     public void setEmail(String email) {
-         this.email = email;
-     }
-
-     @Override
-     public String toString() {
-         return "ContactInfo{" +
-                 "firstName='" + firstName + '\'' +
-                 ", lastName='" + lastName + '\'' +
-                 ", address='" + address + '\'' +
-                 ", city='" + city + '\'' +
-                 ", state='" + state + '\'' +
-                 ", zip=" + zip +
-                 ", phoneNo=" + phoneNo +
-                 ", email='" + email + '\'' +
-                 '}' + "\n";
-     }
- }
+    @Override
+    public String toString() {
+        return "ContactInfo{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNo=" + phoneNo +
+                ", email='" + email + '\'' +
+                '}' + "\n";
+    }
+}
